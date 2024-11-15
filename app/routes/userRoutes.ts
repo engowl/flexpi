134beb4c0c34f87830eca21a848379df2ebaddb2
tfarhan00 <farhanalryan2@gmail.com>
2024-11-16 06:07:45 +0700

    feat: add api credits

--- app/routes/userRoutes.ts
+++ app/routes/userRoutes.ts
@@ -1,6 +1,7 @@
 import { FastifyInstance } from "fastify";
 import { authMiddleware } from "./middlewares/authMiddleware";
 import { prismaClient } from "../lib/prisma";
+import { clamp } from "../utils/miscUtils";
 
 export const userRoutes = (app: FastifyInstance, _: any, done: any) => {
   app.get(
@@ -16,9 +17,24 @@ export const userRoutes = (app: FastifyInstance, _: any, done: any) => {
           where: {
             id: userId,
           },
+          include: {
+            apiKey: true,
+          },
         });
+
+        const userData = {
+          ...user,
+          apiCredits: clamp(
+            (user?.apiKey?.maxLimit || 0) - (user?.apiKey?.usageCount || 0),
+            0,
+            user?.apiKey?.maxLimit || 0
+          ),
+        };
+
+        delete userData.apiKey;
+
         return {
-          data: user,
+          data: userData,
           message: "Success getting user info",
         };
       } catch (e) {
--- app/utils/miscUtils.ts
+++ app/utils/miscUtils.ts
@@ -6,4 +6,20 @@ export const chunkArray = (array: any[], size: number) => {
   return result;
 };
 
-export const sleep = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
+export const sleep = (ms: number) =>
+  new Promise((resolve) => setTimeout(resolve, ms));
+
+export function clamp(value: number, min: number, max: number) {
+  if (value < min) {
+    return min;
+  }
+  if (value > max) {
+    return max;
+  }
+  return value;
+}
+
+// Example usage
+console.log(clamp(5, 1, 10)); // Output: 5
+console.log(clamp(-1, 0, 10)); // Output: 0
+console.log(clamp(15, 0, 10)); // Output: 10
