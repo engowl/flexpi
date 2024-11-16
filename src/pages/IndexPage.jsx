ace29a053c5562529c835b15f8711b8e709285e4
buwono <buwonodirga@gmail.com>
2024-11-16 12:53:28 +0700

    fix(library): fix ui issue

--- src/pages/IndexPage.jsx
+++ src/pages/IndexPage.jsx
@@ -3,7 +3,7 @@ import Experimental from "../components/shared/Experimental.jsx";
 
 export default function IndexPage() {
   return (
-    <main className="min-h-screen flex flex-col gap-5 pt-32 pb-20 px-5 md:px-10 bg-[#F2F2F2]">
+    <main className="min-h-screen flex flex-col gap-5 pt-32 pb-20 px-5 md:px-10 bg-[#F2F2F2] overflow-hidden">
       <Library />
       <Experimental />
     </main>
